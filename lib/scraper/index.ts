c828be83f954b9a874e2b0df189dd7327bd37f7b
rythm-sachdeva <rythmrko123@gmail.com>
2024-07-01 19:19:48 +0530

    DataBase Setup Done

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
-import { extractPrice } from "../utils";
+import { extractPrice,extractCurrency, extractDescription } from "../utils";
 
 export async function scrapeAmazonProduct(url:string){
     if(!url) return;
@@ -36,7 +36,40 @@ export async function scrapeAmazonProduct(url:string){
             $('.a-price.a-text-price')
         );
         // console.log({currentPrice});
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('.a-size-base.a-color-price')
+        );
+        const outOfStock = $('#availability span').text().trim().toLowerCase()=== 'currently unavailable';
+        const images = $('#imgBlkFront').attr('data-a-dynamic-image') || $('#landingImage').attr('data-a-dynamic-image') || '{}';
+        const imageUrls = Object.keys(JSON.parse(images));
+        const currency = extractCurrency($('.a-price-symbol'));
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,"");
+        const description = extractDescription($);
 
+        //constructing data object of parsed data
+        const data = {
+            url,
+            currency: currency || '$',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory : [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount:100,
+            stars: 4.5,
+            isOutOFStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice : Number(originalPrice) || Number(currentPrice),
+            averagePrice : Number(currentPrice) || Number(originalPrice),
+        }
+
+      return data;
     } catch (error:any) {
         throw new Error(`Failed to scrape product: ${error.message}`)
     }
